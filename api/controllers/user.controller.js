cde984450a4e19e7bed2ab5d46e3c254e40f4052
vikram-307 <83663958+vikram-307@users.noreply.github.com>
2024-04-24 13:41:24 +0530

    add signout functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -6,24 +6,24 @@ export const test = (req, res) => {
     res.json({ message: "API is working!" });
 };
 
-export const updateUser = async (req, res, next) => {
-    if(req.params.userId !== req.user.id){
+export const updateUser = async(req, res, next) => {
+    if (req.params.userId !== req.user.id) {
         return next(errorHandler(403, 'You are not allowed to update this user'));
     }
-    if(req.body.password){
-        if(req.body.password.length < 6){
+    if (req.body.password) {
+        if (req.body.password.length < 6) {
             return next(errorHandler(400, 'Password must be atleast 6 characters'));
         }
         req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
-    if(req.body.username){
-        if(req.body.username.length < 7 || req.body.username.length > 20){
+    if (req.body.username) {
+        if (req.body.username.length < 7 || req.body.username.length > 20) {
             return next(errorHandler(400, 'Username must be between 7 and 20 characters'));
         }
-        if(req.body.username.includes(' ')){
+        if (req.body.username.includes(' ')) {
             return next(errorHandler(400, 'Username can not contain spaces'));
         }
-        if(req.body.username !== req.body.username.toLowerCase()){
+        if (req.body.username !== req.body.username.toLowerCase()) {
             return next(errorHandler(400, 'Username must be lowercase'));
         }
         if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
@@ -32,26 +32,24 @@ export const updateUser = async (req, res, next) => {
     }
     try {
         const updatedUser = await User.findByIdAndUpdate(
-          req.params.userId,
-          {
-            $set: {
-              username: req.body.username,
-              email: req.body.email,
-              profilePicture: req.body.profilePicture,
-              password: req.body.password,
-            },
-          },
-          { new: true }
+            req.params.userId, {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    profilePicture: req.body.profilePicture,
+                    password: req.body.password,
+                },
+            }, { new: true }
         );
         const { password, ...rest } = updatedUser._doc;
         res.status(200).json(rest);
     } catch (error) {
         next(error);
     }
-}
+};
 
-export const deleteUser = async (req, res, next) => {
-    if(req.params.userId !== req.user.id){
+export const deleteUser = async(req, res, next) => {
+    if (req.params.userId !== req.user.id) {
         return next(errorHandler(403, 'You are not allowed to delete this user'));
     }
     try {
@@ -60,4 +58,12 @@ export const deleteUser = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const signout = (req, res, next) => {
+    try {
+      res.clearCookie('access_token').status(200).json('User has been signed out');
+    } catch (error) {
+      next(error);
+    }
+  };
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 import { Sidebar } from 'flowbite-react';
 import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { signoutSuccess } from '../redux/user/userSlice';
+import { useDispatch } from 'react-redux';
 
 function DashSidebar() {
     const location = useLocation();
     const [tab, setTab] = useState('');
+    const dispatch = useDispatch();
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
@@ -14,6 +17,23 @@ function DashSidebar() {
         setTab(tabFromUrl);
         }
     }, [location.search]);
+
+    const handleSignout = async () => {
+        try {
+            const res = await fetch('/api/user/signout', {
+                method: 'POST',
+            });
+            const data = await res.json();
+            if (!res.ok) {
+                console.log(data.message);
+            } else {
+                dispatch(signoutSuccess());
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <Sidebar className="w-full md:w-56">
             <Sidebar.Items>
@@ -23,7 +43,7 @@ function DashSidebar() {
                             Profile
                         </Sidebar.Item>
                     </Link>
-                    <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+                    <Sidebar.Item onClick={handleSignout} icon={HiArrowSmRight} className="cursor-pointer">
                         Sign out
                     </Sidebar.Item>
                 </Sidebar.ItemGroup>
