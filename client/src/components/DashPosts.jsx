a64d1634e5ef1cef3c5b5ca9eb3fca998ff1b5b7
vikram-307 <83663958+vikram-307@users.noreply.github.com>
2024-04-27 12:36:14 +0530

    add delete post functionality to the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,13 +1,16 @@
-import { Button, Table } from 'flowbite-react';
+import { Button, Modal, Table } from 'flowbite-react';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { HiOutlineExclamationCircle }  from 'react-icons/hi';
 
 function DashPosts() {
 
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
   const [showMore, setShowMore] = useState(true);
+  const [ showModal, setShowModal ] = useState(false);
+  const [ postIdToDelete , setPostIdToDelete] = useState('');
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -44,6 +47,28 @@ function DashPosts() {
       console.log(error);
     }
   }
+
+  const handleDeletePost = async () => {
+    setShowModal(false);
+    try {
+      const res = await fetch(
+        `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
+        {
+          method: 'DELETE',
+        }
+      );
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        setUserPosts((prev) =>
+          prev.filter((post) => post._id !== postIdToDelete)
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark;scrollbar-thumb-slate-500">
@@ -118,9 +143,33 @@ function DashPosts() {
         </>
       ) : (
         <p>You have no posts yet!</p>
-      )}   
+      )}  
+      <Modal
+                show={showModal}
+                onClose={() => setShowModal(false)}
+                popup
+                size='md'
+            >
+                <Modal.Header />
+                <Modal.Body>
+                <div className='text-center'>
+                    <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+                    <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+                    Are you sure you want to delete this post?
+                    </h3>
+                    <div className='flex justify-center gap-4'>
+                    <Button color='failure' onClick={handleDeletePost}>
+                        Yes, I'm sure
+                    </Button>
+                    <Button color='gray' onClick={() => setShowModal(false)}>
+                        No, cancel
+                    </Button>
+                    </div>
+                </div>
+                </Modal.Body>
+            </Modal> 
     </div>
-  )
+  );
 }
 
 export default DashPosts
