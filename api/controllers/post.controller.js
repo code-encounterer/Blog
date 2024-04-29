947f03a35f1e236a2fa9975f9ac2effea84c8a49
vikram-307 <83663958+vikram-307@users.noreply.github.com>
2024-04-29 16:22:09 +0530

    complete post page functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -65,7 +65,7 @@ export const deletepost = async(req, res, next) => {
 };
 
 export const updatepost = async(req, res, next) => {
-    if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    if (!req.user.isAdmin || req.user.id !== req.params.userId) {
         return next(errorHandler(403, 'You are not allowed to update this post'));
     }
     try {
@@ -76,9 +76,9 @@ export const updatepost = async(req, res, next) => {
                     content: req.body.content,
                     category: req.body.category,
                     image: req.body.image,
-                }
+                },
             }, { new: true }
-        )
+        );
         res.status(200).json(updatedPost);
     } catch (error) {
         next(error);
