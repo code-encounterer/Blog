6d0168f563dd8df56512469ae94862bfad9eca1c
vikram-307 <83663958+vikram-307@users.noreply.github.com>
2024-05-01 11:29:56 +0530

    add delete functionality to the comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
-import { Button, Textarea } from 'flowbite-react'
+import { Button, Textarea } from 'flowbite-react';
 
-function Comment({ comment, onLike, onEdit }) {
+function Comment({ comment, onLike, onEdit, onDelete }) {
     const [user, setUser] = useState({});
     const [isEditing, setIsEditing] = useState(false);
     const [editedContent, setEditedContent] = useState(comment.content);
@@ -21,34 +21,34 @@ function Comment({ comment, onLike, onEdit }) {
             } catch (error) {
                 console.log(error.message);
             }
-        }
+        };
         getUser();
     }, [comment]);
 
     const handleEdit = () => {
         setIsEditing(true);
         setEditedContent(comment.content);
-      };
+    };
     
-      const handleSave = async () => {
+    const handleSave = async () => {
         try {
-          const res = await fetch(`/api/comment/editComment/${comment._id}`, {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              content: editedContent,
-            }),
-          });
-          if (res.ok) {
-            setIsEditing(false);
-            onEdit(comment, editedContent);
-          }
+            const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    content: editedContent,
+                }),
+            });
+            if (res.ok) {
+                setIsEditing(false);
+                onEdit(comment, editedContent);
+            }
         } catch (error) {
-          console.log(error.message);
+            console.log(error.message);
         }
-      };
+    };
 
     return (
         <div className="flex p-4 border-b border-gray-600 text-sm">
@@ -69,29 +69,33 @@ function Comment({ comment, onLike, onEdit }) {
                         </div>
                     </>
                 ) : (
-                <>
-                    <p className="text-gray-500 mb-2">{comment.content}</p>
-                    <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
-                    <button type='button' onClick={() => onLike(comment._id)}
-                    className={`text-gray-400 hover:text-blue-500 ${ currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500' }`}
-                >
-                    <FaThumbsUp className='text-sm' />
-                </button>
-                <p className="text-gray-400">{comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")}</p>
-                {
-                    currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                        <button  onClick={handleEdit} type="button" className="text-gray-400 hover:text-blue-500" >
-                            Edit
-                        </button>
-                    )
-                }
-                    </div>
-                </>
-                ) }
-                
+                    <>
+                        <p className="text-gray-500 mb-2">{comment.content}</p>
+                        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+                            <button
+                                type='button'
+                                onClick={() => onLike(comment._id)}
+                                className={`text-gray-400 hover:text-blue-500 ${ currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500' }`}
+                            >
+                                <FaThumbsUp className='text-sm' />
+                            </button>
+                            <p className="text-gray-400">{comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")}</p>
+                            { currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                                <>
+                                    <button type='button' onClick={handleEdit} className='text-gray-400 hover:text-blue-500'>
+                                        Edit
+                                    </button>
+                                    <button type='button' onClick={() => onDelete(comment._id)} className='text-gray-400 hover:text-red-500'>
+                                        Delete
+                                    </button>
+                                </>
+                            )}
+                        </div>
+                    </>
+                )}
             </div>
         </div>
-    )
+    );
 }
 
-export default Comment
+export default Comment;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,14 +1,17 @@
-import { Button, Textarea } from 'flowbite-react';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import Comment from './Comment';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Button, Modal, Textarea } from 'flowbite-react';
 
 function CommentSection({ postId }) {
     const { currentUser } = useSelector((state) => state.user);
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [commentToDelete, setCommentToDelete] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -20,70 +23,89 @@ function CommentSection({ postId }) {
                     setComments(data);
                 }
             } catch (error) {
-                console.log(error.mesage);
+                console.log(error.message);
             }
-        }
+        };
         getComments();
     }, [postId]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200) {
-          return;
+            return;
         }
         try{
             const response = await fetch('/api/comment/create', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ content: comment, postId, userId: currentUser._id})
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ content: comment, postId, userId: currentUser._id})
             });
             const data = await response.json();
             if (response.ok) {
                 setComment('');
                 setCommentError(null);
-                setComments([data, ...comments])
+                setComments([data, ...comments]);
             }
-        }catch(error){
-            setCommentError(error.mesage);
+        } catch(error){
+            setCommentError(error.message);
         }
-      };
+    };
 
-      const handleLike = async (commentId) => {
+    const handleLike = async (commentId) => {
         try {
-          if (!currentUser) {
-            navigate('/sign-in');
-            return;
-          }
-          const res = await fetch(`/api/comment/likeComment/${commentId}`, {
-            method: 'PUT',
-          });
-          if (res.ok) {
-            const data = await res.json();
-            setComments(
-              comments.map((comment) =>
-                comment._id === commentId
-                  ? {
-                      ...comment,
-                      likes: data.likes,
-                      numberOfLikes: data.likes.length,
-                    }
-                  : comment
-              )
-            );
-          }
+            if (!currentUser) {
+                navigate('/sign-in');
+                return;
+            }
+            const response = await fetch(`/api/comment/likeComment/${commentId}`, {
+                method: 'PUT',
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setComments(
+                    comments.map((comment) =>
+                        comment._id === commentId
+                            ? {
+                                ...comment,
+                                likes: data.likes,
+                                numberOfLikes: data.likes.length,
+                            }
+                            : comment
+                    )
+                );
+            }
         } catch (error) {
-          console.log(error.message);
+            console.log(error.message);
         }
-      };
+    };
 
     const handleEdit = async (comment, editedContent) => {
-    setComments(
-        comments.map((c) =>
-        c._id === comment._id ? { ...c, content: editedContent } : c
-        )
-    );
+        setComments(
+            comments.map((c) =>
+                c._id === comment._id ? { ...c, content: editedContent } : c
+            )
+        );
+    };
+
+    const handleDelete = async (commentId) => {
+        setShowModal(false);
+        try {
+            if (!currentUser) {
+                navigate('/sign-in');
+                return;
+            }
+            const response = await fetch(`/api/comment/deleteComment/${commentId}`, {
+                method: 'DELETE',
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setComments(comments.filter((comment) => comment._id !== commentId));
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
     };
 
     return (
@@ -109,14 +131,12 @@ function CommentSection({ postId }) {
                         <p className="text-gray-500 test-xs">{200 - comment.length} characters remaining</p>
                         <Button gradientDuoTone="purpleToBlue" type="submit" outline>Submit</Button>
                     </div>
-                    {
-                        commentError && <Alert className="mt-5" color="failure">{commentError}</Alert>
-                    }      
+                    { commentError && <Alert className="mt-5" color="failure">{commentError}</Alert> }
                 </form>
             )}
             {comments.length === 0 ? (
                 <p className="text-sm my-5">No comments yet!</p>
-            ): (
+            ) : (
                 <>
                     <div className="text-sm my-5 flex items-center gap-1">
                         <p>Comments</p>
@@ -125,12 +145,48 @@ function CommentSection({ postId }) {
                         </div>
                     </div>
                     {comments.map(comment => (
-                        <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
+                        <Comment
+                            key={comment._id}
+                            comment={comment}
+                            onLike={handleLike}
+                            onEdit={handleEdit}
+                            onDelete={(commentId) => {
+                                setShowModal(true);
+                                setCommentToDelete(commentId);
+                            }}
+                        />
                     ))}
                 </>
             )}
+            <Modal
+                show={showModal}
+                onClose={() => setShowModal(false)}
+                popup
+                size='md'
+            >
+                <Modal.Header />
+                <Modal.Body>
+                    <div className='text-center'>
+                        <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+                        <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-400'>
+                            Are you sure you want to delete this comment?
+                        </h3>
+                        <div className='flex justify-center gap-4'>
+                            <Button
+                                color='failure'
+                                onClick={() => handleDelete(commentToDelete)}
+                            >
+                                Yes, I'm sure
+                            </Button>
+                            <Button color='gray' onClick={() => setShowModal(false)}>
+                                No, cancel
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
         </div>
-    )
+    );
 }
 
 export default CommentSection;
